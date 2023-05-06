8a6de28ed8ae5434b74d4163dcc15e59b0518800
Momin Rather <momin.rather@gmail.com>
2023-05-06 13:47:06 +0530

    error Object

--- registerUser/app.js
+++ registerUser/app.js
@@ -3,27 +3,39 @@ const app = express();
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false })); // to send data from form
 // app.use(express.static("./public/"));
-let message = "";
+
+let errorMessage = {};
 
 app.post("/register", (req, res) => {
+  let regex = new RegExp(
+    /^([a-zA-z0-9_\.\-])+\@(([a-zA-z0-9])+\.)+([a-zA-z0-9]{2,4})+$/g
+  );
   const { name, email, contact, dob } = req.body;
+  console.log(name.length);
   if (name.length == 0) {
-    message += "Name Feild Was Empty\n";
-  }
-  if (contact.length == 0) {
-    message += "Contact Feild was Empty";
-  } else {
-    if (!/^\d+$/.test(contact)) {
-      message += "Contact Should Contain Only Number\n";
-    }
-  }
-  if (message.length == 0) {
-    message = "Registration Successfull";
-  } else {
-    message += "Registration Unsuccessfull.";
+    errorMessage.name = "Name Field Can't be empty";
   }
 
-  res.json(message);
+  if (contact.length != 10) {
+    errorMessage.contact = "Contact Should be of 10 digits";
+  } else {
+    if (!/^\d+$/.test(contact)) {
+      errorMessage.contact += " Contact Should Contain Only Number";
+    }
+  }
+
+  if (!regex.test(email)) {
+    errorMessage.email = "Email Invalid";
+  }
+
+  if (errorMessage.name && errorMessage.contact && errorMessage.email) {
+    errorMessage.result = "Registration Unsuccessfull";
+  } else {
+    errorMessage.result = "Registration Successfull.";
+  }
+
+  res.json(errorMessage);
+  // errorMessage = {};
   //res.json(req.body);
 });
 app.listen(3000, () => console.log("Register User"));
