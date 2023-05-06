23e66b2b9268870bfe8791a17f1153383146fcf7
Momin Rather <momin.rather@gmail.com>
2023-05-06 11:05:14 +0530

    Register User Post Method

---
+++ registerUser/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("./public/"));
+let message = "";
+
+app.post("/register", (req, res) => {
+  const { name, email, contact, dob } = req.body;
+  if (name.length == 0) {
+    message += "Name Feild Was Empty\n";
+  }
+  if (contact.length == 0) {
+    message += "Contact Feild was Empty";
+  } else {
+    if (!/^\d+$/.test(contact)) {
+      message += "Contact Should Contain Only Number\n";
+    }
+  }
+  if (message.length == 0) {
+    message = "Registration Successfull";
+  } else {
+    message += "Registration Unsuccessfull.";
+  }
+
+  res.json(message);
+  //res.json(req.body);
+});
+app.listen(3000, () => console.log("Register User"));
